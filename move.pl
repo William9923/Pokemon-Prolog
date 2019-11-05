77a9af175b064865d733869b86713ece617f2fac
William <williamong1400@gmail.com>
2019-11-05 19:23:55 +0700

    hai

---
+++ move.pl
@@ -0,0 +1,102 @@
+/*** MOVE - move procedure ***/
+/* Moving up */
+w :-
+    checkStart(0),
+    write('You have not started the game.'), !.
+
+w :- letak_player(_,Y),
+	edge(_,[_,YMax]),
+	Y =:= YMax,
+   	write('You cannot go there!'), nl, !.
+
+w :- letak_player(X,Y),
+    Ynew is Y + 1,
+    terrain(river,X,Ynew),
+    write('You cannot go there!'), nl, !.
+
+w :- letak_player(X,Y),
+    Ynew is Y + 1,
+    terrain(block,X,Ynew),
+    write('You cannot go there!'), nl, !.
+
+w :- letak_player(X,Y),
+    retract(letak_player(X,Y)),
+    Ynew is Y + 1,
+    asserta(letak_player(X,Ynew)),!.
+
+/* Moving down */
+s :-
+    checkStart(0),
+    write('You have not started the game.'), !.
+
+s :- letak_player(_,Y),
+	edge(_,[YMin,_]),
+	Y =:= YMin,
+    write('You cannot go there!'), nl, !.
+
+s :- letak_player(X,Y),
+    Ynew is Y - 1,
+    terrain(river,X,Ynew),
+    write('You cannot go there!'), nl, !.
+
+s :- letak_player(X,Y),
+    Ynew is Y - 1,
+    terrain(block,X,Ynew),
+    write('You cannot go there!'), nl, !.
+
+s :- letak_player(X,Y),
+    retract(letak_player(X,Y)),
+    Ynew is Y - 1,
+    asserta(letak_player(X,Ynew)), !.
+
+/* Moving right */
+d :-
+    checkStart(0),
+    write('You have not started the game.'), !.
+
+d :- letak_player(X,_),
+	edge([_,XMax],_),
+	X =:= XMax,
+    write('You cannot go there!'), nl, !.
+
+d :- letak_player(X,Y),
+    Xnew is X + 1,
+    terrain(river,Xnew,Y),
+    write('You cannot go there!'), nl, !.
+
+d :- letak_player(X,Y),
+    Xnew is X + 1,
+    terrain(block,Xnew,Y),
+    write('You cannot go there!'), nl, !.
+
+
+d :- letak_player(X,Y),
+    retract(letak_player(X,Y)),
+    Xnew is X + 1,
+    asserta(letak_player(Xnew,Y)),!.
+
+/* Moving left */
+a :-
+    checkStart(0),
+    write('You have not started the game.'), !.
+
+a :- letak_player(X,_),
+	edge([XMin,_],_),
+	X =:= XMin,
+    write('You cannot go there!'), nl, !.
+
+a :- letak_player(X,Y),
+    Xnew is X - 1,
+    terrain(river,Xnew,Y),
+    write('You cannot go there!'), nl, !.
+
+a :- letak_player(X,Y),
+    Xnew is X - 1,
+    terrain(block,Xnew,Y),
+    write('You cannot go there!'), nl, !.
+
+a :- letak_player(X,Y),
+    retract(letak_player(X,Y)),
+    Xnew is X - 1,
+    asserta(letak_player(Xnew,Y)),!.
+	
