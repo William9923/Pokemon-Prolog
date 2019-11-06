3e43651f12b70f42fbbb5ea4c05ec9e32114118f
William <williamong1400@gmail.com>
2019-11-06 07:56:24 +0700

    commit

---
+++ monsta.pl
@@ -0,0 +1,128 @@
+/* TYPE WIND */
+/* Stats: High Speed and Defese, Low Attack*/
+monsta('Dragonflymon').
+monsta_health('Dragonflymon', 60).
+monsta_attack('Dragonflymon', 40).
+monsta_defense('Dragonflymon', 100).
+monsta_special_attack('Dragonflymon', 40).
+monsta_speed('Dragonflymon', 100).
+monsta_affinity_type('Dragonflymon', Wind).
+
+monsta('Falconmon').
+monsta_health('Falconmon', 60).
+monsta_attack('Falconmon', 20).
+monsta_defense('Falconmon', 80).
+monsta_special_attack('Falconmon', 60).
+monsta_speed('Falconmon', 120).
+monsta_affinity_type('Falconmon', Wind).
+
+monsta('GreatOwlmon').
+monsta_health('GreatOwlmon', 60).
+monsta_attack('GreatOwlmon', 40).
+monsta_defense('GreatOwlmon', 100).
+monsta_special_attack('GreatOwlmon', 60).
+monsta_speed('GreatOwlmon', 60).
+monsta_affinity_type('GreatOwlmon', Wind).
+
+monsta('Slyphmon').
+monsta_health('Slyphmon', 60).
+monsta_attack('Slyphmon', 40).
+monsta_defense('Slyphmon', 100).
+monsta_special_attack('Slyphmon', 60).
+monsta_speed('Slyphmon', 100).
+monsta_affinity_type('Slyphmon', Wind).
+
+monsta('Legendary Emerald Hawk').
+monsta_health('Legendary Emerald Hawk', 80).
+monsta_attack('Legendary Emerald Hawk', 40).
+monsta_defense('Legendary Emerald Hawk', 120).
+monsta_special_attack('Legendary Emerald Hawk', 80).
+monsta_speed('Legendary Emerald Hawk', 120).
+monsta_affinity_type('Legendary Emerald Hawk', Wind).
+
+/* TYPE FIRE */
+/* Elements: Fire, Lightning, and Smoke */
+/* Stats: High Attack and Health, Low Defense*/
+monsta('Cobramon').
+monsta_health('Cobramon', 80).
+monsta_attack('Cobramon', 80).
+monsta_defense('Cobramon', 40).
+monsta_special_attack('Cobramon', 80).
+monsta_speed('Cobramon', 60).
+monsta_affinity_type('Cobramon', Fire).
+
+monsta('KomodoDragonmon').
+monsta_health('KomodoDragonmon', 80).
+monsta_attack('KomodoDragonmon', 80).
+monsta_defense('KomodoDragonmon', 60).
+monsta_special_attack('KomodoDragonmon', 80).
+monsta_speed('KomodoDragonmon', 40).
+monsta_affinity_type('KomodoDragonmon', Fire).
+
+monsta('Magmamon').
+monsta_health('Magmamon', 80).
+monsta_attack('Magmamon', 80).
+monsta_defense('Magmamon', 60).
+monsta_special_attack('Magmamon', 60).
+monsta_speed('Magmamon', 60).
+monsta_affinity_type('Magmamon', Fire).
+
+monsta('Salamandermon').
+monsta_health('Salamandermon', 100).
+monsta_attack('Salamandermon', 100).
+monsta_defense('Salamandermon', 40).
+monsta_special_attack('Salamandermon', 60).
+monsta_speed('Salamandermon', 60).
+monsta_affinity_type('Salamandermon', Fire).
+
+monsta('Legendary Crimson Dragon').
+monsta_health('Legendary Crimson Dragon', 120).
+monsta_attack('Legendary Crimson Dragon', 120).
+monsta_defense('Legendary Crimson Dragon', 40).
+monsta_special_attack('Legendary Crimson Dragon', 80).
+monsta_speed('Legendary Crimson Dragon', 60).
+monsta_affinity_type('Legendary Crimson Dragon', Fire).
+
+
+/* TYPE WATER */
+/* Elements: Water, Void, and Darkness */
+/* Stats: Very High Special Attack, Low Health*/
+monsta('MantaRaymon').
+monsta_health('MantaRaymon', 40).
+monsta_attack('MantaRaymon', 40).
+monsta_defense('MantaRaymon', 40).
+monsta_special_attack('MantaRaymon', 160).
+monsta_speed('MantaRaymon', 40).
+monsta_affinity_type('MantaRaymon', Water).
+
+monsta('GatorGatorGatormon').
+monsta_health('GatorGatorGatormon', 40).
+monsta_attack('GatorGatorGatormon', 60).
+monsta_defense('GatorGatorGatormon', 80).
+monsta_special_attack('GatorGatorGatormon', 100).
+monsta_speed('GatorGatorGatormon', 60).
+monsta_affinity_type('GatorGatorGatormon', Water).
+
+monsta('Orcamon').
+monsta_health('Orcamon', 40).
+monsta_attack('Orcamon', 60).
+monsta_defense('Orcamon', 60).
+monsta_special_attack('Orcamon', 100).
+monsta_speed('Orcamon', 80).
+monsta_affinity_type('Orcamon', Water).
+
+monsta('Undinemon').
+monsta_health('Undinemon', 40).
+monsta_attack('Undinemon', 60).
+monsta_defense('Undinemon', 60).
+monsta_special_attack('Undinemon', 140).
+monsta_speed('Undinemon', 60).
+monsta_affinity_type('Undinemon', Water).
+
+monsta('Legendary Azure Tiger').
+monsta_health('Legendary Azure Tiger', 40).
+monsta_attack('Legendary Azure Tiger', 80).
+monsta_defense('Legendary Azure Tiger', 80).
+monsta_special_attack('Legendary Azure Tiger', 160).
+monsta_speed('Legendary Azure Tiger', 60).
+monsta_affinity_type('Legendary Azure Tiger', Water).
