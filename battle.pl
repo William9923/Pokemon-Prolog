6f1488b04468a95191643641a1b6b3ee5a5e3085
William <williamong1400@gmail.com>
2019-11-06 11:18:17 +0700

    commits

---
+++ battle.pl
@@ -0,0 +1,45 @@
+get_first_monsta([X|_],X).
+
+/* Available instruction */
+battle_inst(attack).
+battle_inst(special_attack).
+battle_inst(withdraw).
+battle_inst(run).
+
+battle(_):-
+	checkStart(0),
+	write('Game has not started. Type "start." to start the game.'),nl,
+	!.
+battle(_) :-
+	write('Entering battle phase'),nl,
+	write('A wild monsta has appeared !!'), nl,
+	list_monsta(L),
+	get_first_monsta(L,Y),
+	format('Go ~a.',[Y]),
+	battle_menu,
+	repeat
+	nl.
+
+battle_menu:-
+	tab(1), write('What are you gonna do? (Type with Prolog syntax'),
+	tab(3),write('attack.'),nl,
+	tab(3),write('special_attack.'),nl,
+	tab(3),write('withdraw.'),nl,
+	tab(3),write('run.'),nl.
+
+/* Validate battle commands */
+battle_user_in(X):-
+	\+battle_inst(X),
+	write('Invalid battle commands. Please try again!'),nl.
+
+battle_user_in(X):-
+	battle_inst(X),
+	X.
+
+attack:-
+
+
+
+
+
+
